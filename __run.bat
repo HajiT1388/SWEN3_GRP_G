ea129aac65e991a6e093079757c185adbca00247
HajiT1388 <if23b185@technikum-wien.at>
2025-09-17 19:04:41 +0200

    Stable OHNE Tests

--- __run.bat
+++ __run.bat
@@ -1,2 +1,4 @@
 docker compose up -d db adminer
 docker compose up -d --build rest
+start http://localhost:9091/?pgsql=db&username=dmsg3&db=dmsg3_db&ns=public
+start http://localhost:8081/swagger/index.html
