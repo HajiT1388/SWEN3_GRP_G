f81c7d74e097cfaf5650e466afee7c9b6d20cc48
HajiT1388 <162899682+HajiT1388@users.noreply.github.com>
2025-10-07 23:30:44 +0200

    Adminer-Öffnungsstring behoben

--- __run.bat
+++ __run.bat
@@ -6,7 +6,7 @@ pushd "%~dp0"
 
 docker compose up --build -d
 
-start "" "http://localhost:9091/?pgsql=db^&username=dmsg3^&db=dmsg3_db^&ns=public"
+start "" "http://localhost:9091/?pgsql=db&username=dmsg3&db=dmsg3_db&ns=public"
 start "" "http://localhost:8081/swagger/index.html"
 start "" "http://localhost:9093/"
 start "" "http://localhost/"
